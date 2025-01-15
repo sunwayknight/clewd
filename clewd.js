66ba3638934d52cf8c24d37baa911fd8f4691b4f
lww <190766630@qq.com>
2025-01-16 03:37:30 +0800

    .

--- clewd.js
+++ clewd.js
@@ -259,6 +259,7 @@ const onListen = async () => {
         Cookie: getCookies()
       }
     });
+    console.log(accRes);
     await checkResErr(accRes);
     const accInfo = (await accRes.json())?.find(item => item.capabilities.includes('chat')); //const accInfo = (await accRes.json())?.[0];\nif (!accInfo || accInfo.error) {\n    throw Error(`Couldn't get account info: "${accInfo?.error?.message || accRes.statusText}"`);\n}\nif (!accInfo?.uuid) {\n    throw Error('Invalid account id');\n}
     setTitle('ok');
